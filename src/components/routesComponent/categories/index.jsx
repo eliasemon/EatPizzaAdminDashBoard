c48759d23df588a1194173f0f62cd7534717c22a
XParif <arifsdarif@gmail.com>
2022-12-05 17:05:28 +0600

    create categories complete

--- src/components/routesComponent/categories/index.jsx
+++ src/components/routesComponent/categories/index.jsx
@@ -1,19 +1,67 @@
 import {
   Box,
+  Button,
   FilledInput,
   FormControl,
   InputLabel,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
   TextField,
 } from "@mui/material";
 
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const Categories = () => {
+  const categoriesList = [
+    {
+      id: "1",
+      title: "Burger",
+    },
+    {
+      id: "2",
+      title: "Pizza",
+    },
+    {
+      id: "3",
+      title: "Sandwich",
+    },
+    {
+      id: "4",
+      title: "Coffee",
+    },
+    {
+      id: "5",
+      title: "Juice",
+    },
+    {
+      id: "6",
+      title: "Kacchi",
+    },
+    {
+      id: "7",
+      title: "Biriyani",
+    },
+    {
+      id: "8",
+      title: "Khichuri",
+    },
+    {
+      id: "9",
+      title: "Tehari",
+    },
+  ];
   return (
     <Box
       sx={{
-        display: "grid",
-        gridTemplateColumns: "17.9vw auto",
+        display: "flex",
+        flexDirection: "column",
+        // gridTemplateColumns: "17.9vw auto",
         width: "100%",
         height: "100%",
+        padding: "3%",
       }}
     >
       <TextField
@@ -35,6 +83,69 @@ const Categories = () => {
           },
         }}
       />
+      <Box sx={{ marginTop: "3%", display: "flex", gap: "2%" }}>
+        <Button variant="contained" size="large">
+          Create
+        </Button>
+        <Button variant="outlined" size="large">
+          Discard
+        </Button>
+      </Box>
+      <Box>
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 400,
+            marginTop: "5%",
+            bgcolor: "secondary",
+            position: "relative",
+            overflow: "auto",
+            color: "common.white",
+            // maxHeight: 300,
+            "& ul": { padding: 0 },
+          }}
+          subheader={<li />}
+        >
+          {
+            <ul>
+              {categoriesList.map((item) => (
+                <ListItem
+                  key={item.id}
+                  sx={{
+                    backgroundColor: "secondary.dark",
+                    marginBottom: "5px",
+                    borderRadius: "5px",
+                  }}
+                >
+                  <ListItemText primary={item.title} />
+                  <Box sx={{ display: "flex", gap: "10px" }}>
+                    <Box>
+                      <EditIcon
+                        sx={{
+                          "&:hover": {
+                            color: "secondary.light",
+                            cursor: "pointer",
+                          },
+                        }}
+                      />
+                    </Box>
+                    <Box>
+                      <DeleteIcon
+                        sx={{
+                          "&:hover": {
+                            color: "secondary.light",
+                            cursor: "pointer",
+                          },
+                        }}
+                      />
+                    </Box>
+                  </Box>
+                </ListItem>
+              ))}
+            </ul>
+          }
+        </List>
+      </Box>
     </Box>
   );
 };
