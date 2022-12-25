0642b09cde7383c8ce0cedd6cfe3bdbc34aa03a4
EliasEmon <019eliashossain@gmail.com>
2022-12-26 00:07:58 +0600

    Resturent Open Closed added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useState , useEffect, useRef } from "react";
 
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
 import Layout from "./Layout";
 import theme from "./constants/theme";
 
@@ -12,15 +11,27 @@ import Background from "./components/backGround";
 import { auth } from "../firebaseConfig";
 
 import { ToastContainer } from "react-toastify";
-
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { findTheResturentStatus } from "../utils/ResturentOpenCloseStatus";
+import { showDataWithOutPagination } from "../utils";
 
 const App = () => {
+
+  const [resturentOpenClosedData , setResturentOpenClosedData] = useState("")
+  const [openingStatus , setOpeningStatus] = useState("")
+  useEffect(() => {
+    showDataWithOutPagination(setResturentOpenClosedData, "ResturentOpeningHr")
+  }, []);
+  useEffect(() => {
+    if(resturentOpenClosedData.length > 0)
+    setOpeningStatus(findTheResturentStatus(resturentOpenClosedData[0].data()))
+  }, [resturentOpenClosedData]);
+
+
+
   const [ui, setUi] = useState(
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    
   );
   // auth.onAuthStateChanged(user=>{
   //   if(user){
@@ -37,7 +48,9 @@ const App = () => {
     <>
       <ThemeProvider theme={theme}>
         <Background>
-          {ui}
+        <BrowserRouter>
+          <Layout openingStatus={openingStatus}  />
+        </BrowserRouter>
           <CssBaseline />
           <ToastContainer theme="dark" />
         </Background>
--- src/components/exploratoryElements/header.jsx
+++ src/components/exploratoryElements/header.jsx
@@ -1,15 +1,17 @@
-import { Button } from "@mui/material";
-import React from "react";
+import { Button , Typography } from "@mui/material";
+import React, { useEffect } from "react";
 import { auth } from "../../../firebaseConfig";
 import LogoContainer from "../logoContainer";
 import { HeaderStyle, LogoWrapper } from "./ExploratoryElements.styled";
 
-const Header = () => {
+const Header = ({openingStatus}) => {
   return (
     <HeaderStyle bgcolor="secondary.deep">
       <LogoWrapper>
         <LogoContainer width="180px" />
       </LogoWrapper>
+
+      <Typography color={openingStatus.status ? "green" : "red"}> {openingStatus.message} </Typography> 
       <Button onClick={() => auth.signOut()}>Logout</Button>
     </HeaderStyle>
   );
