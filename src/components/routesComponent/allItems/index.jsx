fdf96e6274bc0898dcb6bf29d6fc998832ea08ff
MH Murshed <arifsdarif@gmail.com>
2023-01-04 08:18:09 +0600

    all items page complete

--- src/components/routesComponent/allItems/index.jsx
+++ src/components/routesComponent/allItems/index.jsx
@@ -1,15 +1,14 @@
-import { Box, Button, Paper } from "@material-ui/core";
+import { Box, Button } from "@material-ui/core";
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import { useNavigate } from "react-router-dom";
+import ItemList from "../../../constants/ItemsList";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import product from "../../../assets/images/product.jpg";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -50,21 +49,33 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+const ListHeader = styled(Box)`
+  width: 100%;
+  height: 40px;
+  color: #fff;
+  font-weight: 700;
+  background-color: #212020;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+const ListBody = styled(Box)`
+  width: 100%;
+  /* min-height: 40px; */
+  color: #fff;
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+  background-color: #2f2e2e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #212020;
+`;
 
 const AllItems = () => {
   const navigate = useNavigate();
-  
+
   return (
     <Box
       sx={{
@@ -73,64 +84,162 @@ const AllItems = () => {
         // gridTemplateColumns: "17.9vw auto",
         width: "100%",
         height: "100%",
-        padding: "1%",
+        padding: "1.5%",
       }}
     >
-      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Enter user ID or username or phonenumber"
-            inputProps={{ "aria-label": "search" }}
-          />
-        </Search>
-        <Button onClick={() => navigate("/createitem")} variant="contained" size="large" color="primary">
-          Create Items
-        </Button>
-      </Box>
       <Box
         sx={{
-          marginTop: "3%",
+          backgroundColor: "#252525",
+          height: "85%",
+          borderRadius: "5px",
         }}
       >
-        <TableContainer component={Paper}>
-          <Table
-            stickyHeader
-            sx={{
-              minWidth: 650,
-              backgroundColor: "secondary.dark",
-            }}
-            aria-label="simple table"
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "start",
+            padding: "2% 1% 0",
+          }}
+        >
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Enter user ID or username or phonenumber"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Button
+            onClick={() => navigate("/createitem")}
+            variant="contained"
+            size="large"
+            color="primary"
           >
-            <TableHead>
-              <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                  <TableCell align="right">{row.protein}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+            Create Items
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            marginTop: "3%",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              display: "grid",
+              gridTemplateColumns: "1fr 2fr 1fr 1fr 1fr",
+            }}
+          >
+            <ListHeader>Item ID</ListHeader>
+            <ListHeader>Items Name</ListHeader>
+            <ListHeader>Status</ListHeader>
+            <ListHeader>Edit Items</ListHeader>
+            <ListHeader>Delete Items</ListHeader>
+          </Box>
+          <Box
+            sx={{
+              height: "35%",
+              // width: "35%",
+              flex: 1,
+              display: "grid",
+              gridTemplateColumns: "1fr 2fr 1fr 1fr 1fr",
+              overflowY: "scroll",
+            }}
+          >
+            {ItemList.map((item) => (
+              <>
+                <ListBody>{item.id}</ListBody>
+                <ListBody>
+                  <img
+                    src={product}
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      borderRadius: "50%",
+                      marginRight: "1rem",
+                    }}
+                  />
+                  {item.name}
+                </ListBody>
+                <ListBody>{item.isActive ? "True" : "False"}</ListBody>
+                <ListBody>
+                  <EditIcon
+                    onClick={() => console.log("EditIcon clicked")}
+                    sx={{
+                      "&:hover": {
+                        color: "secondary.light",
+                        cursor: "pointer",
+                      },
+                    }}
+                  />
+                </ListBody>
+                <ListBody>
+                  <DeleteIcon
+                    onClick={() => console.log("EditIcon clicked")}
+                    sx={{
+                      "&:hover": {
+                        color: "secondary.light",
+                        cursor: "pointer",
+                      },
+                    }}
+                  />
+                </ListBody>
+              </>
+            ))}
+            {/* {Object.keys(variants).map((id) => {
+              const item = variants[`${id}`];
+              console.log("item", item);
+              return (
+                <>
+                <ListBody>{item.name}</ListBody>
+                <ListBody>{item.regularPrice}</ListBody>
+                <ListBody>{item.sellingPrice}</ListBody>
+                <ListBody>
+                <EditIcon
+                onClick={() =>
+                  setVariantUI(
+                    <AddVariants
+                  incomingItem={item}
+                  onStateLift={onVariantStateLift}
+                  />
+                  )
+                }
+                sx={{
+                  "&:hover": {
+                    color: "secondary.light",
+                    cursor: "pointer",
+                  },
+                }}
+                />
+                </ListBody>
+                <ListBody>
+                <DeleteIcon
+                onClick={() => deleteVariantsHandle(item.id)}
+                sx={{
+                  "&:hover": {
+                    color: "secondary.light",
+                    cursor: "pointer",
+                  },
+                }}
+                />
+                </ListBody>
+                </>
+                // {defualtVariant.id == id && (
+                  //   <ListItemText
+                  //     primary={"Defualt"}
+                  //     sx={{ color: "green" }}
+                  //   />
+                  // )}
+                  );
+                })} */}
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
