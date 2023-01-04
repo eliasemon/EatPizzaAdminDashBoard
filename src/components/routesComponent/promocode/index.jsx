e97ceeaf641616b7abc34b9e8299eca114ad8af4
MH Murshed <arifsdarif@gmail.com>
2023-01-04 10:26:33 +0600

    Promocode page incomplete

--- src/components/routesComponent/promocode/index.jsx
+++ src/components/routesComponent/promocode/index.jsx
@@ -10,120 +10,209 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import {   showDataWithOutPagination , delteColloctionInstance} from "../../../../utils";
 import { useEffect } from "react";
 import CreatePromoCode from "./CreatePromoCode";
+import { PromocodeContainer } from "./Promocode.styled";
+import { CategoryList, HalfBox, ListElement } from "./../../UI/Shape.styled";
+import TitleBar from "./../../UI/TitleBar";
+import { FilterSection } from "../addons/Addons.styled";
+import FilterView from "./../../UI/FilterView";
+import CreateAddons from "./../banner/CreateBanner";
 
 const promoCodeModel = {
-  name : "",
-  description : "",
-  discountType : "",
-  discountValue : "",
-  validity : "",
+  name: "",
+  description: "",
+  discountType: "",
+  discountValue: "",
+  validity: "",
   howManyTimesUsed: "",
-  conditionAmmount : 0
-}
+  conditionAmmount: 0,
+};
 
 const PromoCode = () => {
+  const [showData, setShowData] = useState("");
+  const [createDtaUI, setcreateDtaUI] = useState(
+    <CreatePromoCode EditAbleItem={promoCodeModel} status={false} />
+  );
 
-  const [showData , setShowData] = useState("")
-  const [createDtaUI , setcreateDtaUI] = useState( <CreatePromoCode EditAbleItem={promoCodeModel} status = {false}/>)
+  useEffect(() => {
+    showDataWithOutPagination(setShowData, "promoCode");
+  }, []);
 
-  useEffect(()=>{
-    showDataWithOutPagination( setShowData ,"promoCode")
-  },[])
-
-  const deleteItems = (itemsID , itemName) =>{
-    if(window.confirm(`******************************************** \n\n Wait:= \n\n Do You Wanna Delete \n**"${itemName}"**\PromoCode Item!!! \n\n ********************************************`)){
-      delteColloctionInstance( itemsID ,"promoCode")
+  const deleteItems = (itemsID, itemName) => {
+    if (
+      window.confirm(
+        `******************************************** \n\n Wait:= \n\n Do You Wanna Delete \n**"${itemName}"**\PromoCode Item!!! \n\n ********************************************`
+      )
+    ) {
+      delteColloctionInstance(itemsID, "promoCode");
     }
-    
-  }
-  const clearUi = () =>{
-    setcreateDtaUI(<CreatePromoCode EditAbleItem={promoCodeModel} status = {false} />)
-  }
-  const handelEditDataUi = (item) =>{
-    setcreateDtaUI(<CreatePromoCode EditAbleItem={item} status = {true} clearUi = {clearUi}  />)
-  }
-  
-  
+  };
+  const clearUi = () => {
+    setcreateDtaUI(
+      <CreatePromoCode EditAbleItem={promoCodeModel} status={false} />
+    );
+  };
+  const handelEditDataUi = (item) => {
+    setcreateDtaUI(
+      <CreatePromoCode EditAbleItem={item} status={true} clearUi={clearUi} />
+    );
+  };
 
+  // code stolen from addons
+
+  const [items, setItems] = useState("");
+  const [selectedCatagories, setSelectedCatagories] = useState([]);
+  const clearFilterfun = () => {
+    setSelectedCatagories([]);
+  };
+
+  useEffect(() => {
+    if (selectedCatagories.length > 0) {
+      showDataByArrayQuers(
+        setItems,
+        "Addons",
+        selectedCatagories,
+        "selectedCatagories"
+      );
+    } else {
+      showDataWithOutPagination(setItems, "Addons");
+    }
+  }, [selectedCatagories]);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        // gridTemplateColumns: "17.9vw auto",
-        width: "100%",
-        height: "100%",
-        padding: "3%",
-      }}
-    >
-      
-     
-      {
-        createDtaUI
-      }
-      <Box>
-        <List
-          sx={{
-            width: "100%",
-            maxWidth: 400,
-            marginTop: "5%",
-            bgcolor: "secondary",
-            position: "relative",
-            overflow: "auto",
-            color: "common.white",
-            // maxHeight: 300,
-            "& ul": { padding: 0 },
-          }}
-          subheader={<li />}
-        >
-          {
-            <ul>
-              {showData && showData.map((doc) =>
-              {
-              const item = doc.data()
-              item.id = doc.id ;
-              return(
-                <ListItem
-                  key={doc.id}
-                  sx={{
-                    backgroundColor: "secondary.dark",
-                    marginBottom: "5px",
-                    borderRadius: "5px",
-                  }}
-                >
-                  <ListItemText primary={item.name} />
-                  <Box sx={{ display: "flex", gap: "10px" }}>
+    <PromocodeContainer>
+      <HalfBox color="blue">
+        <TitleBar title="Creation Section" color="blue" />
+        {createDtaUI}
+      </HalfBox>
+      <HalfBox color="green">
+        <TitleBar title="Promocode List" color="green" />
+        {/* <FilterSection>
+          <FilterView
+            selectedCatagories={selectedCatagories}
+            setSelectedCatagories={setSelectedCatagories}
+            clearFilterfun={clearFilterfun}
+            heading="Filter By Catagories"
+          />
+        </FilterSection> */}
+        <CategoryList subheader={<li />}>
+          <ul>
+            {items &&
+              items.map((doc) => {
+                const item = doc.data();
+                item.id = doc.id;
+                return (
+                  <ListElement key={item.id}>
                     <Box>
-                      <EditIcon
-                        onClick = {()=> handelEditDataUi(item)}
-                        sx={{
-                          "&:hover": {
-                            color: "secondary.light",
-                            cursor: "pointer",
-                          },
-                        }}
-                      />
+                      <ListItemText primary={item.name} />
+                      <ListItemText primary={`${item.price} ৳`} />
                     </Box>
-                    <Box>
-                      <DeleteIcon
-                        onClick={() => deleteItems(doc.id , item.name)}
-                        sx={{
-                          "&:hover": {
-                            color: "secondary.light",
-                            cursor: "pointer",
-                          },
-                        }}
-                      />
+                    <Box sx={{ display: "flex", gap: "10px" }}>
+                      <Box>
+                        <EditIcon
+                          onClick={() => handelEditDataUi(item)}
+                          sx={{
+                            "&:hover": {
+                              color: "secondary.light",
+                              cursor: "pointer",
+                            },
+                          }}
+                        />
+                      </Box>
+                      <Box>
+                        <DeleteIcon
+                          onClick={() => deleteItems(item.id, item.name)}
+                          sx={{
+                            "&:hover": {
+                              color: "secondary.light",
+                              cursor: "pointer",
+                            },
+                          }}
+                        />
+                      </Box>
                     </Box>
-                  </Box>
-                </ListItem>
-              )})}
-            </ul>
-          }
-        </List>
-      </Box>
-    </Box>
+                  </ListElement>
+                );
+              })}
+          </ul>
+        </CategoryList>
+      </HalfBox>
+    </PromocodeContainer>
+    // <Box
+    //   sx={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     // gridTemplateColumns: "17.9vw auto",
+    //     width: "100%",
+    //     height: "100%",
+    //     padding: "3%",
+    //   }}
+    // >
+
+    //   {
+    //     createDtaUI
+    //   }
+    //   <Box>
+    //     <List
+    //       sx={{
+    //         width: "100%",
+    //         maxWidth: 400,
+    //         marginTop: "5%",
+    //         bgcolor: "secondary",
+    //         position: "relative",
+    //         overflow: "auto",
+    //         color: "common.white",
+    //         // maxHeight: 300,
+    //         "& ul": { padding: 0 },
+    //       }}
+    //       subheader={<li />}
+    //     >
+    //       {
+    //         <ul>
+    //           {showData && showData.map((doc) =>
+    //           {
+    //           const item = doc.data()
+    //           item.id = doc.id ;
+    //           return(
+    //             <ListItem
+    //               key={doc.id}
+    //               sx={{
+    //                 backgroundColor: "secondary.dark",
+    //                 marginBottom: "5px",
+    //                 borderRadius: "5px",
+    //               }}
+    //             >
+    //               <ListItemText primary={item.name} />
+    //               <Box sx={{ display: "flex", gap: "10px" }}>
+    //                 <Box>
+    //                   <EditIcon
+    //                     onClick = {()=> handelEditDataUi(item)}
+    //                     sx={{
+    //                       "&:hover": {
+    //                         color: "secondary.light",
+    //                         cursor: "pointer",
+    //                       },
+    //                     }}
+    //                   />
+    //                 </Box>
+    //                 <Box>
+    //                   <DeleteIcon
+    //                     onClick={() => deleteItems(doc.id , item.name)}
+    //                     sx={{
+    //                       "&:hover": {
+    //                         color: "secondary.light",
+    //                         cursor: "pointer",
+    //                       },
+    //                     }}
+    //                   />
+    //                 </Box>
+    //               </Box>
+    //             </ListItem>
+    //           )})}
+    //         </ul>
+    //       }
+    //     </List>
+    //   </Box>
+    // </Box>
   );
 };
 export default PromoCode;
